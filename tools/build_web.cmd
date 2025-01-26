21db43094f334aad9955140bded1c6835e803d50
Marek Kulik <noreply@bug32.com>
2025-01-26 18:36:01 +0100

    Initial commit

---
+++ tools/build_web.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+:: Our environment variables provided by Cloudflare Pages:
+:: CI - Always set to true when building for Cloudflare Pages
+:: CI_PREVIEW - Set to true when building a preview of the site
+
+:: Predefined variables by Cloudflare Pages can be found here:
+:: https://developers.cloudflare.com/pages/configuration/build-configuration/#environment-variables
