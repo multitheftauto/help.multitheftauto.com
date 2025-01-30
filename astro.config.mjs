28558365a2138e52325ec574ae4b852eea34d87d
Marek Kulik <noreply@bug32.com>
2025-01-30 02:09:47 +0100

    Switch to Starlight with MTA theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,51 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import * as defaults from './src/constants/defaults';
+import starlight from '@astrojs/starlight';
+import mtasaStarlightThemePlugin from '@multitheftauto/starlight-theme-mtasa';
+import * as constants from './src/content.constants';
+
+const isDeploy = process.env.CI === 'true';
+const siteBaseUrl = isDeploy ? constants.SITE_URL : undefined;
 
 export default defineConfig({
-    site: defaults.SITE_URL,
+    site: siteBaseUrl,
+    integrations: [
+        starlight({
+            plugins: [mtasaStarlightThemePlugin()],
+            title: constants.SITE_TITLE,
+            favicon: '/favicon.ico',
+            logo: {
+                src: './src/assets/logo.png',
+            },
+            titleDelimiter: '—',
+            pagination: false,
+            editLink: {
+                baseUrl: 'https://github.com/multitheftauto/help.multitheftauto.com/edit/main/',
+            },
+            social: {
+                github: 'https://github.com/multitheftauto/help.multitheftauto.com',
+                discord: 'https://discord.com/invite/mtasa',
+                youtube: 'https://youtube.com/user/mtaqa',
+                twitch: 'https://twitch.tv/mtaqa',
+                'x.com': 'https://x.com/mtaqa',
+            },
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [
+                        { label: 'Example Guide', slug: 'guides/example' },
+                    ],
+                },
+                {
+                    label: 'Reference',
+                    autogenerate: { directory: 'reference' },
+                },
+                {
+                    label: 'Troubleshooting',
+                    autogenerate: { directory: 'sa/trouble' },
+                    collapsed: true,
+                },
+            ],
+        }),
+    ],
 });
